e63b7c055af619e01ecb2c085e4971c92c2ba396
kfuentesb <kevfueben@gmail.com>
2026-01-21 11:33:47 +0100

    Proyecto creado

---
+++ src/main/java/com/nextindie/api/NextIndieApplication.java
@@ -0,0 +1,13 @@
+package com.nextindie.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextIndieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextIndieApplication.class, args);
+	}
+
+}
