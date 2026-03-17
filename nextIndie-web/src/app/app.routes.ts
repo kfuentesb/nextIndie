0177820049029e2972080fcd23f6a72fd80e8352
kfuentesb <miraclehawk3@gmail.com>
2026-03-17 17:47:01 +0100

    Modfiicando las routes

--- nextIndie-web/src/app/app.routes.ts
+++ nextIndie-web/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { authGuard} from './core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./features/feed/feed.module').then(m => m.FeedModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
