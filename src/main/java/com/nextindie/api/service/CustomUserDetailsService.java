a7c696e63c8c7a004e5e3982e725834a3e599d68
kfuentesb <miraclehawk3@gmail.com>
2026-03-17 12:32:50 +0100

    Login Request + Jwt + custonuserDetailService

--- src/main/java/com/nextindie/api/service/CustomUserDetailsService.java
+++ src/main/java/com/nextindie/api/service/CustomUserDetailsService.java
@@ -1,4 +1,31 @@
 package com.nextindie.api.service;
 
-public class CustomUserDetailsService {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + usernameOrEmail));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPasswordHash(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole()))
+        );
+    }
 }
