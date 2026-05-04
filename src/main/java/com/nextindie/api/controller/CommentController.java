6eb3e688fe0f163d74f1d9dfd46b0a4a9f9deaa0
kfuentesb <kevfueben@gmail.com>
2026-05-04 22:52:37 +0200

    Arreglo del puerto del cors de 5173 a 5174

--- src/main/java/com/nextindie/api/controller/CommentController.java
+++ src/main/java/com/nextindie/api/controller/CommentController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/comments")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5174")
 public class CommentController {
 
     private final CommentService commentService;
--- src/main/java/com/nextindie/api/controller/GameController.java
+++ src/main/java/com/nextindie/api/controller/GameController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/games")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5174")
 public class GameController {
 
     private final GameService gameService;
