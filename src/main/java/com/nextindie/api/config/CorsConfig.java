55cbbbc57f1f056b9dc4bd31857a98158dfb2a34
kfuentesb <kevfueben@gmail.com>
2026-05-04 22:43:35 +0200

    Fix cors para puerto 5174

--- src/main/java/com/nextindie/api/config/CorsConfig.java
+++ src/main/java/com/nextindie/api/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
